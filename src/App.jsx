327faa5960e68a08fa4576bff2f919e8a0077494
dev-rjw <rjw0105@gmail.com>
2024-09-11 12:39:49 +0900

    2번 중간 완료

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,37 @@ function App() {
         });
     };
 
+    const getProfile = async () => {
+        const response = await axios.get("http://localhost:4000/profile");
+        return response.data;
+    };
+
+    const getComments = async (id) => {
+        const response = await axios.get("http://localhost:4000/comments/" + id);
+        return response.data;
+    };
+
     const { data, isLoading, isError } = useQuery({
         queryKey: ["posts"],
         queryFn: getPosts,
+    });
+
+    const {
+        data: profileData,
+        isLoading: profileIsLoading,
+        isError: profileIsError,
+    } = useQuery({
+        queryKey: ["profile"],
+        queryFn: getProfile,
+    });
+
+    const {
+        data: commentsData,
+        isLoading: commentsIsLoading,
+        isError: commentsIsError,
+    } = useQuery({
+        queryKey: ["comments", id],
+        queryFn: getComments(id),
     });
 
     const mutation = useMutation({
@@ -41,6 +69,14 @@ function App() {
         return <div>오류가 발생하였습니다...</div>;
     }
 
+    if (profileIsLoading) {
+        return <div>로딩중입니다...</div>;
+    }
+
+    if (profileIsError) {
+        return <div>오류가 발생하였습니다...</div>;
+    }
+
     return (
         <>
             <input value={title} onChange={(e) => setTitle(e.target.value)} />
@@ -49,10 +85,12 @@ function App() {
             {data.map((post) => {
                 return (
                     <div key={post.id}>
-                        {post.title} (views: {post.views})
+                        {post.title} (views: {post.views})<button onClick={() => getComments(post.id)}>댓글보기</button>
                     </div>
                 );
             })}
+            <hr />
+            {profileData.name}
         </>
     );
 }
